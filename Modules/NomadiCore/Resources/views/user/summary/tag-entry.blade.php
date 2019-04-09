26e793f4851e99a5847d36ff0f1721f6577ccf83
isccorg12 <isccorg12@gmail.com>
2019-04-09 09:14:18 +0000

    modify the readme to be the correct source web pages

--- Modules/NomadiCore/Resources/views/user/summary/tag-entry.blade.php
+++ Modules/NomadiCore/Resources/views/user/summary/tag-entry.blade.php
@@ -1,6 +1,8 @@
 <div class='entry'>
     對
-    <a href='/shop/{{$cafeTag->entity['id']}}'><span style='font-weight: bold;'>{{ $cafeTag->entitytags['name'] }}</span></a>
+    {{$cafeTag}}
+    {{$cafeTag->entity_id}}
+    <a href='/shop/{{$cafeTag->entitytags['id']}}'><span style='font-weight: bold;'>{{ $cafeTag->entitytags['name'] }}</span></a>
     加上標籤：
     {{$cafeTag->tag->name}}
 
